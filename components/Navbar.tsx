73ad446d870de0b37187375d96ec33d3819bd25d
Jesdakorn Saelor <jesdakorns@hotmail.com>
2021-03-29 15:45:05 +0700

    update css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,6 +52,7 @@ const useStyles = makeStyles({
                 flexDirection: 'column',
                 width: '100%',
                 height: '100vh',
+                height: 'calc(var(--vh, 1vh) * 100)',
                 left: 0,
                 top: 0,
                 
