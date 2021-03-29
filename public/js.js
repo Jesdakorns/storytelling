6826cc580f49bd3e83bf6a4919537ede6b1c77c2
Jesdakorn Saelor <jesdakorns@hotmail.com>
2021-03-29 17:55:48 +0700

    update nav

--- public/js.js
+++ public/js.js
@@ -1,6 +1,6 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener('resize',function(){
 let vh = window.innerHeight * 0.01;
-console.log(vh);
 document.documentElement.style.setProperty("--vh", `${vh}px`);
-console.log(--vh);
 });
