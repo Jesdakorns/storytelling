5b1e24a3f60c0c27de8449d9ae453e935a800e17
Jesdakorn <jesdakorns@hotmail.com>
2021-06-29 04:19:43 +0700

    no message

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     publicRuntimeConfig: {
         // Will be available on both server and client
-        API_BASE: 'http://7f39c3a9b8c2.ngrok.io/',
-        API_HOST: 'http://7f39c3a9b8c2.ngrok.io/api/',
+        API_BASE: 'https://8e9cd47204b1.ngrok.io/',
+        API_HOST: 'https://8e9cd47204b1.ngrok.io/api/',
     },
 }
